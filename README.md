d852eeef993bb94b8244519282839f29a4b56bca
Stephen Kilbourn <skilbourn@element84.com>
2022-11-07 13:52:57 -0600

    follow steps

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# streak-counter
-a streak counter for the browser, inspired by Duolingo
+# `@skilbourn/streak-counter` - a basic streak counter
+
+This is a basic streak counter - inspired by Duolingo - written in TypeScript and meant for the browser (uses `localStorage`).
+
+## Install
+
+```shell
+yarn add @skilbourn/streak-counter
+```
+
+
+## Usage
+
+import {streakCounter} from '@skilbourn/streak-counter'
+
+const today = new Date()
+const streak = streakCounter(localStorage, today)
+// streak returns an object:
+// {
+//    currentCount: 1,
+//    lastLoginDate: "11/05/2022",
+//    startDate: "11/05/2022",
+// }
---
+++ src/utils.ts
@@ -0,0 +1,35 @@
+// Used when storing in localStorage
+export const KEY = 'streak'
+
+export function formattedDate(date: Date): string {
+  // returns date as 11/11/2021
+  // other times it returns 11/11/2021, 12:00:00 AM
+  // which is why we call the .split at the end
+  return date.toLocaleDateString('en-US')
+}
+
+export interface Streak {
+  currentCount: number
+  startDate: string
+  lastLoginDate: string
+}
+
+export function buildStreak(
+  date: Date,
+  overrideDefaults?: Partial<Streak>,
+): Streak {
+  const defaultStreak = {
+    currentCount: 1,
+    startDate: formattedDate(date),
+    lastLoginDate: formattedDate(date),
+  }
+
+  return {
+    ...defaultStreak,
+    ...overrideDefaults,
+  }
+}
+
+export function updateStreak(storage: Storage, streak: Streak): void {
+  storage.setItem(KEY, JSON.stringify(streak))
+}
